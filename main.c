74cb758f2e36f75a04efa5a4120657be66a3084f
Jacob Hemmerich <jake@hemmerich.co>
2018-10-27 16:26:58 -0500

    Update main.c
    Co-authored-by: Joshua Kalyanapu <jkalyanapu@users.noreply.github.com>

--- main.c
+++ main.c
@@ -7,6 +7,24 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/types.h>
+
+/*
+ * @source: https://github.com/systemsdevelopmentclub/user-interface
+ * @license: https://www.gnu.org/licenses/lgpl.txt
+ * @contact: https://www.stuorg.iastate.edu/site/sdc/information
+ * Copyright (c) 2018 Systems Development Club
+ * This file is part of SDC Search.
+ * SDC Search is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * SDC Search is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with SDC Search.  If not, see <https://www.gnu.org/licenses/>.
+ */
  
 int main(void)
 {
